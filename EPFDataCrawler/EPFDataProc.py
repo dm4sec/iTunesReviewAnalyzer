d7bccdab11b4db170cec0020f0f5d82c13574f47
dm4sec <ellison.tang@gmail.com>
2021-06-29 15:23:17 +0800

    fix a few bugs

--- EPFDataCrawler/EPFDataProc.py
+++ EPFDataCrawler/EPFDataProc.py
@@ -10,14 +10,23 @@ from EPFDownloader.epf_downloader_full import EPFDowloaderFull
 from EPFDownloader.epf_downloader_incremental import EPFDowloaderIncremental
 
 def main():
+
+
     parser = argparse.ArgumentParser()
     parser.add_argument('--username', required=True, help="Username for accessing EPF data.")
     parser.add_argument('--password', required=True, help="Password for accessing EPF data.")
 
     args = parser.parse_args()
-    epfDataDownloaderFull = EPFDowloaderFull(args.username, args.password, "../EPFDataCache")
 
-    epfDataDownloaderFull.perform_download()
+    EPFDataPath = "./EPFData"
+    if not os.path.exists(EPFDataPath):
+        os.mkdir(EPFDataPath)
+
+    epfDataDownloaderFull = EPFDowloaderFull(args.username, args.password, EPFDataPath)
+
+    r = epfDataDownloaderFull.perform_download()
+    if r != "":
+        print("foo")
 
 
 if __name__ == "__main__":
